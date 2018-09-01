961ea399b6b94836f0231ac5c49a9c28af7c59a1
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-09-01 12:03:46 -0300

    change name, pkey to table

--- sql/create_registry.sql
+++ sql/create_registry.sql
@@ -1,15 +1,15 @@
--- Table: public.registry
+-- Table: public.root_hashes
 
--- DROP TABLE public.registry;
+-- DROP TABLE public.root_hashes;
 
 CREATE TABLE public.registry
 (
     ipfs_hash VARCHAR(46) NOT NULL, --IPFS hash
     identity VARCHAR(64), -- identity - it can be either an ethereum addr or did
-    CONSTRAINT registry_pkey PRIMARY KEY (ipfs_hash,identity)
+    CONSTRAINT registry_pkey PRIMARY KEY (identity)
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE public.registry
+ALTER TABLE public.root_hashes
   OWNER TO root;
