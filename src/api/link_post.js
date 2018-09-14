c30c7069afdbbb42f734d8d97b8789c0afa26826
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-09-14 14:00:15 -0300

    verify linked_did with the one on the message
    remove extra console.log

--- src/api/link_post.js
+++ src/api/link_post.js
@@ -1,47 +1,59 @@
 class LinkPostHandler {
-  constructor(sigMgr,linkMgr) {
-    this.sigMgr = sigMgr;
-    this.linkMgr = linkMgr;
+  constructor (sigMgr, linkMgr) {
+    this.sigMgr = sigMgr
+    this.linkMgr = linkMgr
   }
-  async handle(event, context, cb) {
-    //Parse body
-    let body;
+  async handle (event, context, cb) {
+    // Parse body
+    let body
     try {
-      body = JSON.parse(event.body);
+      body = JSON.parse(event.body)
     } catch (e) {
-      cb({ code: 403, message: "no json body: " + e.toString() });
-      return;
+      cb({ code: 403, message: 'no json body: ' + e.toString() })
+      return
     }
 
     // Check if data is present
     if (!body.consent_signature) {
-      cb({ code: 403, message: "no consent_signature" });
-      return;
+      cb({ code: 403, message: 'no consent_signature' })
+      return
     }
     if (!body.consent_msg) {
-      cb({ code: 403, message: "no consent_msg" });
-      return;
+      cb({ code: 403, message: 'no consent_msg' })
+      return
     }
     if (!body.linked_did) {
-      cb({ code: 403, message: "no linked_did" });
-      return;
+      cb({ code: 403, message: 'no linked_did' })
+      return
     }
 
-    const sig = body.consent_signature;
-    const msg = body.consent_msg;
-    const did = body.linked_did;
-    
-    //Get address from sigsignature + msg
-    const address=await this.sigMgr.verify(msg,sig);
+    const sig = body.consent_signature
+    const msg = body.consent_msg
+    const did = body.linked_did
+
+    const regex = /Your unique profile ID is (.*)/
+
+    let msg_did = regex.exec(msg)
+    if (!msg_did) {
+      cb({ code: 400, message: 'no did on the consent_msg' })
+      return
+    }
+
+    if (msg_did[1] !== did) {
+      cb({ code: 400, message: 'dids does not match' })
+      return
+    }
+
+    // Get address from sigsignature + msg
+    const address = await this.sigMgr.verify(msg, sig)
     const consent = {
       sig: sig,
       msg: msg
     }
 
-    await this.linkMgr.store(address, did, JSON.stringify(consent));
+    await this.linkMgr.store(address, did, JSON.stringify(consent))
 
-    cb(null,{did: did, address: address});
-    return;
+    cb(null, { did: did, address: address })
   }
 }
-module.exports = LinkPostHandler;
+module.exports = LinkPostHandler
