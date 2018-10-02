e740badab29b2bb5cd64c41dc3fa4eed6827623e
Cristóbal Castillo <1154738+c-castillo@users.noreply.github.com>
2018-10-02 16:29:08 -0300

    fix length of orbitdb address path (#31)

--- sql/create_root_store_addresses.sql
+++ sql/create_root_store_addresses.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE public.root_store_addresses
 (
-    root_store_address VARCHAR(46) NOT NULL, --OrbitDB root store address
+    root_store_address VARCHAR(200) NOT NULL, --OrbitDB root store address
     did VARCHAR(64), -- did
     CONSTRAINT root_store_addresses_pkey PRIMARY KEY (did)
 )
