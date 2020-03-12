54188cfc6de9ce9c056fc320b90252626a1e2940
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2020-03-12 13:20:17 +0100

    chore: use default serializers

--- src/logger.js
+++ src/logger.js
@@ -2,9 +2,6 @@ const bunyan = require('bunyan')
 
 const defaultOptions = {
   codeVersion: process.env.CODE_VERSION,
-  serializers: {
-    err: bunyan.stdSerializers.err
-  },
 }
 
 module.exports.createLogger = (opts) => bunyan.createLogger(Object.assign({}, defaultOptions, opts))
